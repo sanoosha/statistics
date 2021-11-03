019e4bdebcd9972702c6824730f6920067e8ba25
sindhu madhavan <sindhua77@gmail.com>
2021-11-03 12:15:50 -0500

    added two-tail hypothesis test for two proportions

--- stats.py
+++ stats.py
@@ -1,8 +1,9 @@
 from scipy.stats import norm
+import scipy.stats.distributions as dist
 import argparse
 import numpy as np
 import pandas as pd
-from statsmodels.stats.proportion import confint_proportions_2indep
+from statsmodels.stats.proportion import confint_proportions_2indep, test_proportions_2indep
 
 def confidence_interval_diff_proportions(c1, s1, c2, s2, a):
 
@@ -18,6 +19,20 @@ def confidence_interval_diff_proportions(c1, s1, c2, s2, a):
     low_interval, upp_interval = confint_proportions_2indep(c1, s1, c2, s2, method=None, compare='diff', alpha=a)
 
     return low_interval, upp_interval
+
+def hypothesis_test_diff_proportions(c1, s1, c2, s2):
+
+    '''
+    Assume two-tail test; Will add 
+    functionality for left or right tail 
+    test.
+    '''
+
+    print(c1, s1, c2, s2)
+
+    statistic, pval = test_proportions_2indep(c1, s1, c2, s2, compare='diff', alternative='two-sided', return_results=False)
+
+    return statistic, pval
 
 if __name__ == '__main__':
 
@@ -37,9 +52,15 @@ if __name__ == '__main__':
 
     samples_statistic = (count1/samples1) - (count2/samples2)
     low_interval, upp_interval = confidence_interval_diff_proportions(count1, samples1, count2, samples2, alpha)
+    statistic, pval = hypothesis_test_diff_proportions(count1, samples1, count2, samples2)
 
     print('Sample Statistic :')
     print("{:.3f}".format(samples_statistic))
 
     print('Range of interval :')
     print("{:.3f}".format(low_interval)," - " , "{:.3f}".format(upp_interval))
+
+    print('Two-tailed Hypothesis Test  Statistic :', "{:.3f}".format(statistic))
+    print('Two-tailed Hypothesis Test  p-value :', "{:.3f}".format(pval))
+    # test_pval = 2*dist.norm.cdf(-np.abs(statistic))
+    # print(test_pval)
