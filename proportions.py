1824e5600e14e86e86af2b57c19c93d31ce5fb58
Sindhu Madhavan <sindhu.madhavan@evonik.com>
2022-01-11 08:42:36 -0600

    updated proportions.py

--- proportions.py
+++ proportions.py
@@ -95,47 +95,47 @@ def one_proportions_confidence_interval(c1, n1, confidence_interval, tails=2):
 
 if __name__ == '__main__':
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-c1', '--count1', help='Enter number of successes of group 1.')
-    # parser.add_argument('-n1', '--samples1', help='Enter number of samples from group 1.')
-    # parser.add_argument('-c2', '--count2', help='Enter number of success of group 2.')
-    # parser.add_argument('-n2', '--samples2', help='Enter number of samples from group 2.')
-    # parser.add_argument('-ci', '--confidence_interval', help='Enter confidence interval.')
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c1', '--count1', help='Enter number of successes of group 1.')
+    parser.add_argument('-n1', '--samples1', help='Enter number of samples from group 1.')
+    parser.add_argument('-c2', '--count2', help='Enter number of success of group 2.')
+    parser.add_argument('-n2', '--samples2', help='Enter number of samples from group 2.')
+    parser.add_argument('-ci', '--confidence_interval', help='Enter confidence interval.')
+    args = parser.parse_args()
 
-    # ## group 1 values
-    # count1 = float(args.count1)
-    # samples1 = float(args.samples1)
+    ## group 1 values
+    count1 = float(args.count1)
+    samples1 = float(args.samples1)
 
-    # ## group 2 values
-    # count2 = float(args.count2)
-    # samples2 = float(args.samples2)
+    ## group 2 values
+    count2 = float(args.count2)
+    samples2 = float(args.samples2)
 
-    # ## alpha level
-    # alpha = 1 - float(args.confidence_interval)
+    ## alpha level
+    alpha = 1 - float(args.confidence_interval)
 
-    # ## Run statistic functions
-    # samples_statistic = (count1/samples1) - (count2/samples2)
-    # print('Sample Statistic :')
-    # print("{:.3f}".format(samples_statistic))
+    ## Run statistic functions
+    samples_statistic = (count1/samples1) - (count2/samples2)
+    print('Sample Statistic :')
+    print("{:.3f}".format(samples_statistic))
 
-    ## confidence intervals
-    low_interval, upp_interval = confidence_interval_diff_proportions(c1=6, n1=24, c2=14, n2=24, confidence_interval=0.83)
-    print('Range of interval :')
-    print("{:.3f}".format(low_interval)," - " , "{:.3f}".format(upp_interval))
-    print('Length of confidence interval :', "{:.3f}".format(abs(upp_interval-low_interval)))
+    # ## confidence intervals
+    # low_interval, upp_interval = confidence_interval_diff_proportions(c1=6, n1=24, c2=14, n2=24, confidence_interval=0.83)
+    # print('Range of interval :')
+    # print("{:.3f}".format(low_interval)," - " , "{:.3f}".format(upp_interval))
+    # print('Length of confidence interval :', "{:.3f}".format(abs(upp_interval-low_interval)))
     
-    low_interval, upp_interval = two_proportions_confidence_interval(c1=6, n1=24, c2=14, n2=24, confidence_interval=0.83, tails=2)
-    print('Range of interval :')
-    print("{:.3f}".format(low_interval)," - " , "{:.3f}".format(upp_interval))
-    print('Length of confidence interval :', "{:.3f}".format(abs(upp_interval-low_interval)))
+    # low_interval, upp_interval = two_proportions_confidence_interval(c1=6, n1=24, c2=14, n2=24, confidence_interval=0.83, tails=2)
+    # print('Range of interval :')
+    # print("{:.3f}".format(low_interval)," - " , "{:.3f}".format(upp_interval))
+    # print('Length of confidence interval :', "{:.3f}".format(abs(upp_interval-low_interval)))
 
-    # ## hypothesis tests
+    ## hypothesis tests
     # pval = two_proportions_hypothesis_test(c1=6, n1=24, c2=14, n2=24, confidence_interval=0.38, tails=2)
-    # statistic, pval = hypothesis_test_diff_proportions(count1, samples1, count2, samples2)
+    statistic, pval = hypothesis_test_diff_proportions(count1, samples1, count2, samples2)
 
-    # print('Two-tailed Hypothesis Test Statistic :', "{:.3f}".format(statistic))
-    # print('Two-tailed Hypothesis Test p-value :', "{:.3f}".format(pval))
+    print('Two-tailed Hypothesis Test Statistic :', "{:.3f}".format(statistic))
+    print('Two-tailed Hypothesis Test p-value :', "{:.3f}".format(pval))
 
     # ## sample size
     # sample_size = sample_size_single_proportion(confidence_interval=0.90, margin_error=0.06, p=0.25)
